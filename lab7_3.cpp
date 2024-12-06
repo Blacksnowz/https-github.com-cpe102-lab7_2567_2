fa63c86d6a4eabac387f5719191a102c26950b93
Blacksnowz <flaxiaroz@gmail.com>
2024-12-06 11:14:29 +0700

    complete

---
+++ lab7_3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+char before(char x){
+    if (x >='A' && x <= 'Z'){
+        if(x == 'A'){
+            return'Z';
+        }else{
+            return (x-1);
+        }
+    }else{
+        return'0';
+    }
+}
+
